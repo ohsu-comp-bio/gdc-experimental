b05ce6942ec7a6ea858dc7a99050acb4ee3f5bfe
Brian Walsh <brian@bwalsh.com>
2016-11-05 20:44:54 -0700

    rename

--- util/load-es.sh
+++
@@ -1,3 +0,0 @@
-cat aggregated_resource.json | \
-  jq -c '. | {"index": {"_index": "test-aggregated-resource", "_type": "aggregated_resource", "_id": ._id}}, del(._id)' | \
-  curl -XPOST localhost:9200/_bulk --data-binary @-
